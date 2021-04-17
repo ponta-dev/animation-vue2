563451f64286f3071a4c6be1488ed9d55a208b65
ponta-dev <ponta.dev.matumoto@gmail.com>
2021-04-17 15:16:51 +0900

    ファイル関係の記載

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  // ベースパスの設定
+//  publicPath: "/sample/",
+  // npm run serve時のサーバーの設定
+/*  devServer: {
+    // 外部API等を利用する場合のproxy設定
+    proxy: {
+      '^/context': {
+        target: 'http://~~:80',
+        changeOrigin: true
+      }
+    }
+  } */
 }
